9f14cc5c26338accff5d54ad0a611cf9cf3b0c18
ovvn <omazinov@gmail.com>
2019-07-21 20:20:08 +0300

    Add travis file. Update src.

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Osman made a tough decision!
+          Changing this will trigger auto-update!
         </p>
         <a
           className="App-link"
