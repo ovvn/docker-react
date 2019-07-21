71d982da9a48a321dda41e7ba3460d2c98f41848
ovvn <omazinov@gmail.com>
2019-07-21 22:25:03 +0300

    Change app text.

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Changing this will trigger auto-update!
+          I was changed on the feature branch!
         </p>
         <a
           className="App-link"
